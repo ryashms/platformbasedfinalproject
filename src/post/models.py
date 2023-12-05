523056a67d513f3990e6b9211bc5f313a430f6f9
ryashms <130333131+ryashms@users.noreply.github.com>
2023-12-05 17:31:04 -0500

    first commit

---
+++ src/post/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+def upload_location(instance, filename):
+    file_path = 'images/{author_id}/{filename}'.format(
+        author_id=instance.author.id, filename=filename
+    )
+    return file_path
+
+class ImagePost(models.Model):
+    title = models.CharField(max_length=100)
+    image = models.ImageField(upload_to=upload_location)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
